45f0c0125a86f5b81026eaf77c458d5177ecf6ec
Not-Your-Code <vineetagarwal.now@gmail.com>
2023-08-18 16:39:29 +0530

    completed frontend functionality for signup and did some changes with backend too

--- frontend/spotify/src/App.jsx
+++ frontend/spotify/src/App.jsx
@@ -1,5 +1,5 @@
 
-
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage'
@@ -12,13 +12,16 @@ import SignupPage from './pages/signupPage/Signup'
 //everything is function in react , it always return the jsx - javascript and html 
 function App() {
 
+  //loggedin State
+  const [IsLoggedIn , setIsLoggedIn] = useState(false);
+
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/signup" element={<SignupPage/>}/>
+        <Route path="/signup" element={<SignupPage setIsLoggedIn = {setIsLoggedIn}/>}/>
       </Routes>
     </Router>
   )
--- frontend/spotify/src/pages/SignupPage/Signup.jsx
+++ frontend/spotify/src/pages/SignupPage/Signup.jsx
@@ -1,9 +1,22 @@
 
 import { useNavigate } from 'react-router-dom';
 import './Signup.css'
+import PropTypes from 'prop-types';
 import SignForm from '../../utilities/form/signupForm/SignForm';
-export default function SignupPage() {
+import Error from '../../utilities/errorSeg/error';
+import Success from '../../utilities/SuccessSeg/Success';
+import { useState } from 'react';
+// eslint-disable-next-line react/prop-types
+
+
+export default function SignupPage({setIsLoggedIn}) {
    
+
+   const [isErrorExist , setErrorExist] = useState(false);
+   const [error , setError ] = useState("");
+
+   const [isSuccessExist , setSuccessExist] = useState(false);
+   const [success , setSuccess ] = useState("");
 
 
     function handleLoginClick() {
@@ -17,10 +30,21 @@ export default function SignupPage() {
     return (
         <>
             <div className="signup">
+                {isErrorExist ? (
+                    <div id='error'>
+                        <Error error = {error}/>
+                        </div>
+                ) : null}
+
+                {
+                    isSuccessExist ? (
+                       <p>hello</p>
+                    ):null
+                }
                 <div className="header">
                     <img src="./images/Signup.png" alt="" />
                     <h1>Sign up for free to start listening.</h1>
-                      <SignForm/>
+                      <SignForm setIsLoggedIn={setIsLoggedIn} setErrorExist={setErrorExist} setError={setError} setSuccessExist={setSuccessExist}  setSuccess={setSuccess} />
                     <p>Already have an account? <span style={{ textDecoration: "underline", cursor: "pointer" , color:"green"  }} onClick={() => {
                         handleLoginClick()
                     }}>Log in</span></p>
@@ -36,3 +60,7 @@ export default function SignupPage() {
         </>
     )
 }
+
+SignForm.propTypes = {
+   setIsLoggedIn: PropTypes.func, // Ensure setLoggedIn is a required function prop
+  };
